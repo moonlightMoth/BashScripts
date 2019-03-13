4e80657844140b51e1335a2cf27d23ac0fa21419
moonlightMoth <ghjpa@inbox.ru>
2019-03-13 19:55:19 +0300

    added cmds to customCommands.sh

--- customCommands.sh
+++ customCommands.sh
@@ -5,3 +5,15 @@ function ca()
 {
 	cd ./$1; la
 }
+
+# read line specified by number from file and open it in firefox
+function olink()
+{
+	sed -n $1p $2 | xargs firefox
+}
+
+# kek
+function blyad()
+{
+	echo 'Calm down, everything ok, continiue trying'
+}
