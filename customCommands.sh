889569b67f7636a1425ca41a9a3f49511c169f9b
moonlightMoth <ghjpa@inbox.ru>
2019-03-13 23:02:16 +0300

    ss

--- customCommands.sh
+++ customCommands.sh
@@ -9,18 +9,26 @@ function ca()
 # read line specified by number from file and open it in firefox
 function olink()
 {
-	sed -n $1p $2 | xargs firefox
+	sed -n $1p $2 | xargs firefox;
+	return 0;
 }
 
 # kek
 function blyad()
 {
 	echo 'Calm down, everything ok, keep trying'
+	return 0;
 }
 
 # commits all changes to git with specified message and pushes to remote
 function gitpush()
 {
+	if [ $# -gt 1 ]
+	then
+		echo 'Erroe: requires only one argument - commit message';
+		return 1
+	fi
+
 	if ! [ $# -eq 0 ]
 	then
 		git commit -a -m $1
@@ -30,6 +38,7 @@ function gitpush()
 			git push
 		fi
 	else
-		echo 'Error: commit message not specified'
+		echo 'Error: commit message not specified';
+		return 1
 	fi
 }
