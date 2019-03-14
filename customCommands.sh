46da577191dda04f2e7b9ee32851be9fbc0ab485
moonlightMoth <ghjpa@inbox.ru>
2019-03-14 05:24:21 +0300

    moved tygidig into customCommands

--- customCommands.sh
+++ customCommands.sh
@@ -48,6 +48,31 @@ function gitpush()
 	return 1
 }
 
+tygdig()
+{
+	for i in {1..1000};
+	do
+		sleep 0.25;
+
+		for j in $(seq 1 $[$i % 19]);
+		do
+			echo -n -e "\t|" ;
+		done;
+
+		if [ $[$i % 19] -ne 0 ]
+		then
+			echo -n "тыг-дык|"
+		fi
+
+		for k in $(seq 1 $[19 - $i % 19]);
+		do
+			echo -e -n "\t|";
+		done;
+
+		echo -e -n "\n";
+	done
+}
+
 # ummm
 saas()
 {
